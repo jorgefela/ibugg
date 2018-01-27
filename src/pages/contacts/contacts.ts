d4c2e62b59693df4d2321c6b7464b89c1a3a3c29
Raymundo Salser <raymundo@vmctechnology.com>
2018-01-26 19:09:08 -0600

    26012018 - Commit 01

--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
+
+import { GetDataProvider } from '../../providers/get-data/get-data';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the ContactsPage page.
@@ -8,18 +13,64 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-contacts',
-  templateUrl: 'contacts.html',
-})
-export class ContactsPage {
+ @IonicPage()
+ @Component({
+ 	selector: 'page-contacts',
+ 	templateUrl: 'contacts.html',
+ })
+ export class ContactsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+ 	contacts: any = [];
+ 	username = '';
+ 	email = '';
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactsPage');
-  }
 
-}
+ 	constructor(public navCtrl: NavController, private getDataService: GetDataProvider, private auth: AuthServiceProvider, private storage : Storage, public events: Events) {
+ 		this.auth.showLoading('Please Wait..');
+
+ 		let info:any;
+		this.storage.get('userid').then((val) => {
+			// info = val;
+			if(val != undefined){
+				// console.log(val);
+				info = [{
+					auth : true,
+					userid : val
+				}];
+			}else{
+				info = this.auth.getUserInfo();
+			}
+
+			if(info != undefined && info[0]['auth'] == true){
+				// this.navCtrl.setRoot('HomePage')
+				this.auth.closeLoading();
+				this.getContacts(info[0]['userid']);
+				// console.log(info[0]['userid']);
+			}else{
+				this.navCtrl.setRoot('LoginPage')
+				this.auth.closeLoading();
+			}
+		});
+ 	}
+
+ 	getContacts(userid){
+ 		this.getDataService.getData('http://ibugg2.vmcgraphics.com/api/contacts/?userid='+userid+'&status=1').subscribe(
+ 			data => this.contacts = data
+ 			);
+ 	}
+
+ 	changeStatus(id, column, value){
+ 		this.getDataService.getData('http://ibugg2.vmcgraphics.com/api/contacts/update/?id='+id+'&column='+column+'&value='+value+'').subscribe(
+ 			data => {
+ 				if(data.status == true){
+ 					this.navCtrl.setRoot(this.navCtrl.getActive().component);
+ 				}
+ 			}
+ 			);
+ 	}
+
+ 	public editContact(id){
+        this.events.publish('user:edit', data['userid'], Date.now());
+        this.navCtrl.setRoot('EditContactPage');
+ 	}
+ }
